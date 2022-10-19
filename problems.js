957201c75413027cc8193d5f57c80a93490cd148
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2022-10-19 12:27:02 -0400

    adding starting code

---
+++ problems.js
@@ -0,0 +1,16 @@
+const problems = {
+  "countingDNANucleotides":{
+    testData: {
+      type:"string",    
+      payload:"AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC",    
+      // type:"file",
+      // payload:"rosalind_dna_1_dataset.txt",
+      // postLoad: () => {
+      //   currentProblem.data = currentProblem.data[0]
+      // }
+    },
+    text: "Counting DNA Nucleotides",
+    f: countingDNANucleotides
+  },
+  
+}
--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-const colorList = ['maroon','brown','olive','teal','navy','black','red','orange','yellow','lime','green','cyan','blue','purple','magenta','grey','pink','apricot','beige','mint','lavender','white'];
+let currentProblem = {};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  stroke(random(colorList));
-  fill(random(colorList));  
   background(255);
+  noLoop();
 }
 
 function draw() {
-  ellipse(mouseX, mouseY, 25, 25);
+  background("white");
+  const x = 10;
+  let y = 10;
+  let dy = 20;
+  for( let k of Object.keys(bindings) ) {
+    text( `${k}: ${problems[bindings[k]].text}`, x, y );
+    y += dy;
+  }
+  if( currentProblem.data ) {
+    currentProblem.f();
+  }
 }
+
+const bindings = {
+  "c":"countingDNANucleotides", // values need to match keys in the problems object
+}
+
+function keyPressed() {
+  if( key in bindings ) {
+    currentProblem = problems[bindings[key]]
+    loadProblemData();
+  }
+}
+
+function loadProblemData() {
+  if( currentProblem.testData.type === "string" ) {
+    currentProblem.data = currentProblem.testData.payload;
+    redraw();
+  } else if( currentProblem.testData.type === "file" ) {
+    currentProblem.data = loadStrings( 
+      currentProblem.testData.payload, 
+      () => {
+        if( currentProblem.testData.postLoad ) {
+          currentProblem.testData.postLoad()
+        }
+        redraw()
+      }
+    );
+  }
+}
+
